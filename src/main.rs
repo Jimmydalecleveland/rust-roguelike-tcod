e2652ed37783151fb9e1ad1b271790ef667935fa
Jimmy DC <jimmydalecleveland@gmail.com>
2022-07-31 09:51:36 -0600

    add tunnel functions and make horizontal one

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use tcod::colors::*;
 use tcod::console::*;
+use std::cmp;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -170,6 +171,7 @@ fn make_map() -> Map {
     let room2 = Rect::new(50, 15, 10, 15);
     create_room(room1, &mut map);
     create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
 
     map
 }
@@ -208,5 +210,18 @@ fn create_room(room: Rect, map: &mut Map) {
         for y in (room.y1 + 1)..room.y2 {
             map[x as usize][y as usize] = Tile::empty();
         }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    // horizontal tunnel. `min()` and `max()` are used in case `x1 > x2`
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
     }
 }
