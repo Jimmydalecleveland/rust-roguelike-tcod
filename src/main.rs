d8f89734aa4d374ede417d770a45fef43907ff98
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2022-09-12 10:19:18 -0600

    update Object to support alive and blocking player
    This required some refactoring of the `move_by` method on Object
    to avoid borrowing conflicts.
    
    `move_by` has been moved out of Object as a plain function.
    
    `handle_keys` was updated to accept Map for `move_by`, and all objects instead of only player
    
    finally, `place_objects` now accepts a `Map` and checks if a spot is blocked on the map before creating a monster

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
+use rand::Rng;
+use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
 use tcod::map::{FovAlgorithm, Map as FovMap};
-use std::cmp;
-use rand::Rng;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
-const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
-const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
-const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50 };
+const COLOR_LIGHT_WALL: Color = Color {
+    r: 130,
+    g: 110,
+    b: 50,
+};
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+const COLOR_LIGHT_GROUND: Color = Color {
+    r: 200,
+    g: 180,
+    b: 50,
+};
 const ROOM_MAX_SIZE: i32 = 10;
 const ROOM_MIN_SIZE: i32 = 6;
 const MAX_ROOMS: i32 = 30;
@@ -36,22 +48,25 @@ struct Object {
     y: i32,
     char: char,
     color: Color,
+    name: String,
+    blocks: bool,
+    alive: bool,
 }
 
 impl Object {
-    pub fn new (x: i32, y: i32, char: char, color: Color) -> Self {
-        Object { x, y, char, color }
-    }
-
-    /// move by the given amount
-    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
-        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
-            self.x += dx;
-            self.y += dy;
+    pub fn new(x: i32, y: i32, char: char, name: &str, color: Color, blocks: bool) -> Self {
+        Object {
+            x,
+            y,
+            char,
+            name: name.into(),
+            color,
+            alive: false,
+            blocks,
         }
     }
 
-    /// set the color and then draw the character that represents 
+    /// set the color and then draw the character that represents
     /// this object at its position
     pub fn draw(&self, con: &mut dyn Console) {
         con.set_default_foreground(self.color);
@@ -67,7 +82,6 @@ impl Object {
         self.y = y;
     }
 }
-
 
 /// Tile for map and it's properties
 #[derive(Clone, Copy, Debug)]
@@ -112,7 +126,7 @@ impl Rect {
             y2: y + h,
         }
     }
-    
+
     pub fn center(&self) -> (i32, i32) {
         let center_x = (self.x1 + self.x2) / 2;
         let center_y = (self.y1 + self.y2) / 2;
@@ -123,14 +137,12 @@ impl Rect {
         // If either of these conditions is false, then the rectangles cannot intersect.
         // If both of these conditons are true, then the rectangles can intersect
         // on the X axis.
-        let intersects_on_x_axis = (self.x1 <= other.x2) 
-            && (self.x2  >= other.x1);
+        let intersects_on_x_axis = (self.x1 <= other.x2) && (self.x2 >= other.x1);
 
         // If either of these conditions is false, then the rectangles cannot intersect.
         // If both of these conditons are true, then the rectangles can intersect
         // on the Y axis.
-        let intersects_on_y_axis = (self.y1 <= other.y2) 
-            && (self.y2 >= other.y1);
+        let intersects_on_y_axis = (self.y1 <= other.y2) && (self.y2 >= other.y1);
 
         // If both of these conditions are true, then the rectangles intersect.
         intersects_on_x_axis && intersects_on_y_axis
@@ -138,10 +150,10 @@ impl Rect {
 }
 
 /// NOTE:
-/// There’s a ton of different ways to create the map. 
-/// One common alternative is one continuous Vec with MAP_HEIGHT * MAP_WIDTH items. 
-/// To access a tile on (x, y), you would do map[y * MAP_WIDTH + x]. 
-/// The advantage is that you only do one array lookup instead of two and iterating 
+/// There’s a ton of different ways to create the map.
+/// One common alternative is one continuous Vec with MAP_HEIGHT * MAP_WIDTH items.
+/// To access a tile on (x, y), you would do map[y * MAP_WIDTH + x].
+/// The advantage is that you only do one array lookup instead of two and iterating
 /// over every object in the map is faster because they’re all in the same region of memory.
 type Map = Vec<Vec<Tile>>;
 
@@ -163,7 +175,8 @@ fn main() {
     let mut tcod = Tcod { root, con, fov };
 
     // Set up player, npc and vector of objects (players are objects)
-    let player = Object::new(0, 0, '@', WHITE);
+    let mut player = Object::new(0, 0, '@', "player", WHITE, true);
+    player.alive = true;
     let mut objects = vec![player];
 
     let mut game = Game {
@@ -183,7 +196,7 @@ fn main() {
     }
 
     // force FOV "recompute" first time through the game loop
-    let mut previous_player_position = (-1, -1);
+    let previous_player_position = (-1, -1);
 
     // Game loop
     while !tcod.root.window_closed() {
@@ -191,32 +204,34 @@ fn main() {
 
         let fov_recompute = previous_player_position != (objects[PLAYER].x, objects[PLAYER].y);
         render_all(&mut tcod, &mut game, &objects, fov_recompute);
-                
+
         tcod.root.flush();
 
-
-        let player = &mut objects[PLAYER];
-        let exit = handle_keys(&mut tcod, &game, player);
+        let exit = handle_keys(&mut tcod, &game.map, &mut objects);
         if exit {
             break;
         }
     }
 }
 
-fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
+fn handle_keys(tcod: &mut Tcod, map: &Map, objects: &mut [Object]) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
     let key = tcod.root.wait_for_keypress(true);
     match key {
         // movement keys
-        Key { code: Up, .. } => player.move_by(0, -1, game),
-        Key { code: Down, .. } => player.move_by(0, 1, game),
-        Key { code: Left, .. } => player.move_by(-1, 0, game),
-        Key { code: Right, .. } => player.move_by(1, 0, game),
-        
+        Key { code: Up, .. } => move_by(PLAYER, 0, -1, map, objects),
+        Key { code: Down, .. } => move_by(PLAYER, 0, 1, map, objects),
+        Key { code: Left, .. } => move_by(PLAYER, -1, 0, map, objects),
+        Key { code: Right, .. } => move_by(PLAYER, 1, 0, map, objects),
+
         // toggle fullscreen
-        Key { code: Enter, alt: true, .. } => {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
             let fullscreen = tcod.root.is_fullscreen();
             tcod.root.set_fullscreen(!fullscreen);
         }
@@ -242,11 +257,13 @@ fn make_map(objects: &mut Vec<Object>) -> Map {
 
         let new_room = Rect::new(x, y, w, h);
 
-        let is_valid_location = !rooms.iter().any(|other_room| new_room.intersects_with(other_room));
+        let is_valid_location = !rooms
+            .iter()
+            .any(|other_room| new_room.intersects_with(other_room));
 
         if is_valid_location {
             create_room(new_room, &mut map);
-            place_objects(new_room, objects);
+            place_objects(new_room, &map, objects);
 
             let (new_x, new_y) = new_room.center();
 
@@ -256,7 +273,7 @@ fn make_map(objects: &mut Vec<Object>) -> Map {
             } else {
                 // center coordinates of previous room
                 let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
-                
+
                 // coinflip (50/50 bool)
                 if rand::random() {
                     create_h_tunnel(prev_x, new_x, prev_y, &mut map);
@@ -279,7 +296,8 @@ fn render_all(tcod: &mut Tcod, game: &mut Game, objects: &[Object], fov_recomput
     // recompute FOV if needed (the player moved or something)
     if fov_recompute {
         let player = &objects[PLAYER];
-        tcod.fov.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
+        tcod.fov
+            .compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
     }
     // draw all objects in the list
     for object in objects {
@@ -307,7 +325,8 @@ fn render_all(tcod: &mut Tcod, game: &mut Game, objects: &[Object], fov_recomput
                 *is_explored = true;
             }
             if *is_explored {
-                tcod.con.set_char_background(x, y, color, BackgroundFlag::Set);
+                tcod.con
+                    .set_char_background(x, y, color, BackgroundFlag::Set);
             }
         }
     }
@@ -344,23 +363,48 @@ fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
     }
 }
 
-fn place_objects(room: Rect, objects: &mut Vec<Object>) {
+fn place_objects(room: Rect, map: &Map, objects: &mut Vec<Object>) {
     // choose random number of monsters
     let num_monsters = rand::thread_rng().gen_range(0, MAX_ROOM_MONSTERS + 1);
 
     for _ in 0..num_monsters {
         // choose random spot for this monster
+        // gen_range is inclusive for arg1 and exclusive for arg2,
+        // so we need to add 1 to the first argument to not put a
+        // monster in a wall.
         let x = rand::thread_rng().gen_range(room.x1 + 1, room.x2);
         let y = rand::thread_rng().gen_range(room.y1 + 1, room.y2);
 
-        let mut monster = if rand::random::<f32>() < 0.8 { // 80% chance of getting an orc
-            // Orc
-            Object::new(x, y, 'o', DESATURATED_GREEN)
-        } else {
-            // Troll
-            Object::new(x, y, 'V', DARK_RED)
-        };
+        if !is_blocked(x, y, map, objects) {
+            let mut monster = if rand::random::<f32>() < 0.8 {
+                // 80% chance of getting an orc
+                Object::new(x, y, 'o', "orc", DESATURATED_GREEN, true)
+            } else {
+                Object::new(x, y, 'V', "Vampire", DARK_RED, true)
+            };
 
-        objects.push(monster);
+            monster.alive = true;
+            objects.push(monster);
+        }
+    }
+}
+
+fn is_blocked(x: i32, y: i32, map: &Map, objects: &[Object]) -> bool {
+    // first test the map tile
+    if map[x as usize][y as usize].blocked {
+        return true;
+    }
+
+    // now check for any blocking objects
+    objects
+        .iter()
+        .any(|object| object.blocks && object.pos() == (x, y))
+}
+
+/// move by the given amount
+fn move_by(id: usize, dx: i32, dy: i32, map: &Map, objects: &mut [Object]) {
+    let (x, y) = objects[id].pos();
+    if !is_blocked(x + dx, y + dy, map, objects) {
+        objects[id].set_pos(x + dx, y + dy);
     }
 }
